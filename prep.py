9330063153a18359fb7495bb3de789210f76089b
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-06-06 14:42:21 -0400

    Added arg parsing with click, allowing for wildcarded inputs. Fixed some abs/rel path issues when creating metadata.

--- prep.py
+++ prep.py
@@ -13,46 +13,80 @@ format (that is, when provided an img it checks for an accompanying hdr).
 # transform into crs of datacube index.
 #
 # TODO: datacube could perform this transformation itself rather than entrusting yamls.
-# This may support more careful consideration of datums, and issues such as the corner 
+# This may support more careful consideration of datums, and issues such as the corner
 # coords failing to enclose the area due to curvature of the projected border segments.
 import rasterio.warp
 from osgeo import osr
+import sys
+import click
+import yaml
+from pathlib import Path
+
+
 def get_geometry(path):
     with rasterio.open(path) as img:
         left, bottom, right, top = img.bounds
         crs = str(str(getattr(img, 'crs_wkt', None) or img.crs.wkt))
         corners = {
-                    'ul': {'x': left, 'y': top},
-                    'ur': {'x': right, 'y': top},
-                    'll': {'x': left, 'y': bottom},
-                    'lr': {'x': right, 'y': bottom} 
-                  }
+            'ul': {
+                'x': left,
+                'y': top
+            },
+            'ur': {
+                'x': right,
+                'y': top
+            },
+            'll': {
+                'x': left,
+                'y': bottom
+            },
+            'lr': {
+                'x': right,
+                'y': bottom
+            }
+        }
         projection = {'spatial_reference': crs, 'geo_ref_points': corners}
 
         spatial_ref = osr.SpatialReference(crs)
         t = osr.CoordinateTransformation(spatial_ref, spatial_ref.CloneGeogCS())
+
         def transform(p):
             lon, lat, z = t.TransformPoint(p['x'], p['y'])
             return {'lon': lon, 'lat': lat}
-        extent = {key: transform(p) for key,p in corners.items()}
+
+        extent = {key: transform(p) for key, p in corners.items()}
 
         return projection, extent
 
 
 # Construct metadata dict
 import uuid
-from xml.etree import ElementTree # should use cElementTree..
+from xml.etree import ElementTree  # should use cElementTree..
 from dateutil import parser
 import os
+
+bands = ['vh', 'vv']
+
+
+def band_name(path):
+    name = path.stem
+    # position = name.find('_')
+    if 'VH' in str(path):
+        layername = 'vh'
+    if 'VV' in str(path):
+        layername = 'vv'
+    return layername
+
+
 def prep_dataset(path):
     # input: path = .dim filename
 
     # Read in the XML header
-
-    xml = ElementTree.parse(str(path)).getroot().find(
-        "Dataset_Sources/MDElem[@name='metadata']/MDElem[@name='Abstracted_Metadata']")
+    xml_path = path.joinpath(path.stem + '.dim')
+    xml = ElementTree.parse(
+        str(xml_path)).getroot().find("Dataset_Sources/MDElem[@name='metadata']/MDElem[@name='Abstracted_Metadata']")
     scene_name = xml.find("MDATTR[@name='PRODUCT']").text
-    platform = xml.find("MDATTR[@name='MISSION']").text.replace('-','_')
+    platform = xml.find("MDATTR[@name='MISSION']").text.replace('-', '_')
     t0 = parser.parse(xml.find("MDATTR[@name='first_line_time']").text)
     t1 = parser.parse(xml.find("MDATTR[@name='last_line_time']").text)
 
@@ -61,47 +95,64 @@ def prep_dataset(path):
     # could read production/productscenerasterstart(stop)time
 
     # get bands
-
     # TODO: verify band info from xml
-    
-    bands = ['vh','vv']
-    bandpaths = [str(os.path.join(path[:-3]+'data','Gamma0_' + pol.upper() + '.img'))
-    			for pol in bands]
-       
+    images = {band_name(im_path): {'path': str(im_path.relative_to(path))} for im_path in path.glob('*.data/*.img')}
+
     # trusting bands coaligned, use one to generate spatial bounds for all
 
-    projection, extent = get_geometry(bandpaths[0])
-    
+    projection, extent = get_geometry('/'.join([str(path), images['vv']['path']]))
+
     # format metadata (i.e. construct hashtable tree for syntax of file interface)
 
     return {
         'id': str(uuid.uuid4()),
-        'processing_level': "terrain",
+        'processing_level': "CEOS_ARD",
         'product_type': "gamma0",
-        #'creation_dt':  t0,
-        'platform': {'code': 'SENTINEL_1'},
-        'instrument': {'name': 'SAR'},
-        'extent': { 'coord': extent, 'from_dt': str(t0), 'to_dt': str(t1), 'center_dt': str(t0+(t1-t0)/2) },
-        'format': {'name': 'ENVI'}, # ENVI or BEAM-DIMAP ?
-        'grid_spatial': {'projection': projection},
-        'image': { 'bands': {b: {'path': p, 'nodata': 0} for b,p in zip(bands,bandpaths)} },
-        'lineage': {'source_datasets': {}, 'ga_label': scene_name} # TODO!
+        'creation_dt': t0,
+        'platform': {
+            'code': 'SENTINEL_1'
+        },
+        'instrument': {
+            'name': 'SAR'
+        },
+        'extent': {
+            'coord': extent,
+            'from_dt': str(t0),
+            'from_dt': str(t1),
+            'center_dt': str(t0 + (t1 - t0) / 2)
+        },
+        'format': {
+            'name': 'ENVI'
+        },  # ENVI or BEAM-DIMAP ?
+        'grid_spatial': {
+            'projection': projection
+        },
+        'image': {
+            'bands': images
+        },
+        'lineage': {
+            'source_datasets': {},
+            'ga_label': scene_name
+        }  # TODO!
         # C band, etc...
     }
 
 
+@click.command(
+    help="Prepare S1A/B data processed with GPT in BEAM-DIMAP format dataset for ingestion into the Data Cube.")
+@click.argument('datasets', type=click.Path(exists=True, readable=True, writable=True), nargs=-1)
+def main(datasets):
+    for dataset in datasets:
+        path = Path(dataset)
+        assert path.glob('*.dim'), "Expect a directory with a BEAM-DIMAP header file as input"
+        print("Starting for dataset " + dataset)
+        metadata = prep_dataset(path)
 
-import sys
-import yaml
+        yaml_path = str(path.joinpath('agdc-metadata.yaml'))
 
-if len(sys.argv) != 2:
-    print("Usage: python prep.py scene.dim")
-    print("or (bulk usage): for file in *.dim; do python prep.py $file; done")
-else:
-    scene = sys.argv[-1]
-    assert scene.lower().endswith('.dim'), "Expect the BEAM-DIMAP header file as input"
-    metadata = prep_dataset(scene)
-    yaml_path = scene[:-3] + 'yaml' # change suffix
+        with open(yaml_path, 'w') as stream:
+            yaml.dump(metadata, stream)
 
-    with open(yaml_path,'w') as stream:
-        yaml.dump(metadata,stream)
+
+if __name__ == "__main__":
+    main()
